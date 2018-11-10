98036044bb2e57505065c77c6f2a95ec4c3645e7
jinerha <673359985@qq.com>
2018-11-10 22:25:19 +0800

    压缩1次

--- js/main.js
+++ js/main.js
@@ -1,67 +1,58 @@
 /*滚动事件开始*/
-$(window).scroll(function() {
-	headerInit()
-})
-function headerInit(){
-	if($(this).scrollTop()>0){
-		$('body').addClass('fiexed-header-on');
-	}else{
-		$('body').removeClass('fiexed-header-on');
-	}
-}
-headerInit();
-var swiper = new Swiper('#banner .swiper-container', {
-  spaceBetween: 30,
-  effect: 'fade',
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  }
-});
-
-var swiper2 = new Swiper('#dif .swiper-container', {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  }
-});
-/*滚动事件结束*/
-
-/* 返回顶部开始 */
 $(function(){
-  $('#goTop').click(function(){
-    $('html').animate({
-      'scrollTop': 0
+  var swiper = new Swiper('#banner .swiper-container', {
+    spaceBetween: 30,
+    effect: 'fade',
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    }
+  });
+
+  new Swiper('#dif .swiper-container', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    }
+  });
+  /*滚动事件结束*/
+
+  /* 返回顶部开始 */
+  $(function(){
+    $('#goTop').click(function(event){
+      $('html').animate({
+        'scrollTop': 0
+      },1000)
+    })
+  })
+  /* 返回顶部结束 */
+
+  /* 滚动监听开始 */
+  $('body').scrollspy({ target: '#bs-example-navbar-collapse-1' })
+  /* 滚动监听结束 */
+
+  /* 平滑滚动开始 */
+  $('.navbar a').click(function(event){
+    let target = $(this.hash)
+    $('html,body').animate({
+      scrollTop: target.offset().top
     },1000)
   })
-})
-/* 返回顶部结束 */
-
-/* 滚动监听开始 */
-$('body').scrollspy({ target: '#mynavbar' })
-/* 滚动监听结束 */
-
-/* 平滑滚动开始 */
-$('.navbar a').click(function(event){
-  let target = $(this.hash)
-  $('html').animate({
-    scrollTop: target.offset().top
-  },1000)
+  /* 平滑滚动结束 */
 })
 
-/* 平滑滚动结束 */
